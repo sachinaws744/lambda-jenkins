3036e2a337556b85b7e3f3f938503acc30b9bbc5
nareshdevopscloud <150413361+nareshdevopscloud@users.noreply.github.com>
2024-01-23 23:03:08 +0530

    Update lambda-pipeline.py

--- lambda-pipeline.py
+++ lambda-pipeline.py
@@ -1,5 +1,5 @@
 import boto3
-lambda_Client = boto3.client('lambda')
+lambda_Client = boto3.client('lambda', region_name='us-east-1')
 response =lambda_Client.create_function(
           Code={
                 'S3Bucket': 'cloudeng-buck',
